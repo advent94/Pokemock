985c3422b29da5d749a9bd3bffa27953d90bc548
advent94 <adventowy@gmail.com>
2024-02-16 22:16:25 +0100

    Added Graphics global class and shader includes
    Globals:
    - Graphics
    
    Graphics global will consist of shader preprocessing utility
    functions. For now I added universal functionalities that can
    be used in future that I developed during my work on Oak's intro
    shaders.
    
    Shader includes contain both utilities and functionalities that
    were created during experiments with shaders to create intro
    shader that were deemed useful and universal enough to include
    them in project. Using these includes it is possible to create
    Oak's intro shader which was done on side.

---
+++ scripts/utils/global/graphics.gd
@@ -0,0 +1,80 @@
+extends Node
+
+func get_pixel_array(img: Image) -> Array[Color]:
+	var pixels: Array[Color] = []
+	var x: int = 0
+	var y: int = 0
+	var img_width: int = img.get_width()
+	var img_height: int = img.get_height()
+	for i in range(0, img_width * img_height):
+		x = i - ((i/img_width) * img_width)
+		y = i / img_width
+		pixels.push_back(img.get_pixel(x, y))
+	return pixels
+
+const OPAQUE: float = 1.0
+
+func get_unique_colors(color_array: Array[Color], remove_transparent: bool = true) -> Array[Color]:
+	var unique_colors: Array[Color] = []
+	for color in color_array:
+		if remove_transparent && not is_equal_approx(color.a, OPAQUE):
+			continue
+		if not unique_colors.has(color):
+			unique_colors.push_back(color)
+	return unique_colors
+
+enum Sort { ASCENDING, DESCENDING }
+
+var COLOR_SORT_INDEX: Dictionary = {
+	Sort.ASCENDING: _sort_ascending_by_luminance,
+	Sort.DESCENDING: _sort_descending_by_luminance
+}
+
+func _sort_ascending_by_luminance(a: Color, b: Color):
+	if a.get_luminance() < b.get_luminance():
+		return true
+	return false
+
+func _sort_descending_by_luminance(a: Color, b: Color):
+	if a.get_luminance() > b.get_luminance():
+		return true
+	return false
+
+func sort_colors(color_array: Array[Color], sort: Sort) -> Array[Color]:
+	color_array.sort_custom(COLOR_SORT_INDEX[sort])
+	return color_array
+
+const MAX_LUMINANCE: float = 1.0
+
+## Finds corresponding color to it's own luminance
+func get_color_by_luminance(color: Color, colors_sorted_by_luminance: Array[Color], sort: Sort = Sort.ASCENDING) -> Color:
+	if colors_sorted_by_luminance.is_empty():
+		return color
+	var mod: float = color.get_luminance()
+	# Basically, we reverse mod by subtracting it's max value from it's current value
+	if sort == Sort.DESCENDING:
+		mod = MAX_LUMINANCE - color.get_luminance()
+	return colors_sorted_by_luminance[(colors_sorted_by_luminance.size() - 1) * mod]
+
+## Limits color count based on existing array of colors and number you want to limit it by
+## Color limit consists of the brightest color, darkest and [count - 2] colors between
+func get_limited_color_array(color_array: Array[Color], count: int) -> Array[Color]:
+	assert(count >= 1, "Color count can't be zero or negative.")
+	
+	if count >= color_array.size():
+		return color_array
+	
+	if count == 1:
+		return [ color_array[0] ]
+
+	var cut_color_array: Array[Color] = []
+	var it_modifier: float = float(color_array.size()) / (count - 1)
+	var iterator: float = 0.0
+
+	while cut_color_array.size() < count - 1:
+		cut_color_array.push_back(color_array[round(iterator)])
+		iterator += it_modifier
+	
+	cut_color_array.push_back(color_array[color_array.size() - 1])
+	
+	return cut_color_array
