5f99042ae6de5c5a94dc5ac97d22d4ce9d8d284d
advent94 <adventowy@gmail.com>
2024-02-25 11:44:15 +0100

    Added Call status check and updated test framework
    Added missing documentation comments for Test Framework.
    
    New Test Framework functionalities:
    
    CALL_STATUS_UPDATE(index) - creates callable that updates status
    under index, simple variable if not specified. This callable is
    further used to be connected to signal or is called after other
    method gets called. When it gets called, call status check will
    return true. Otherwise it returns false.
    
    CALLED(index) - checks if call status was updated (callable was
    called at least once, might support call times in future) for index
    specified (simple variable by default). If it was, CALLED returns
    true, otherwise returns false.
    
    RESET_CALL(index) - sets call status under specified index to false
    (not called, default index points on simple variable)
    
    RESET_ALL_CALLS() - performs reset call on every single call status

--- scripts/tests/framework.gd
+++ scripts/tests/framework.gd
@@ -1,11 +1,13 @@
 extends TestMethods
 
+## Implements all functionalities for test execution.
 class_name Testing
 
 const TEST_CASES_NODE_NAME: String = "TestCases"
 const TEST_PREFIX: String = "should"
 const TESTED_ENTITIES_NODE_NAME: String = "TestedEntity"
 
+## Gets node from TestedEntity tree. Used to easily access, modify entities for testing purposes.
 func get_entity(entity_name: String) -> Node:
 	var test_fixture = get_test_fixture()
 	assert(test_fixture, "Couldn't find test fixture. Make sure that your test scene hierarchy is properly set.")
@@ -14,6 +16,7 @@ func get_entity(entity_name: String) -> Node:
 	assert(tested_entity.has_node(entity_name), "Test fixture(%s) has no entity named: \"%s\"" % [test_fixture.name, entity_name])
 	return tested_entity.get_node(entity_name)
 
+## Returns true when node's name ends with "Test" and it has tree with test cases to run.
 func is_test_fixture(node: Node) -> bool:
 	var children_names = Functions.get_children_names(node)
 	return node.name.ends_with("Test") and children_names.has(TEST_CASES_NODE_NAME)
@@ -22,21 +25,17 @@ func is_test_fixture(node: Node) -> bool:
 # of each test and parse it in object oriented way, print stack for each test case and so on. It's
 # just meant to be generic but simple in a way that we will only base results on Debugger errors for now
 
+## Returns true if class consists only of test fixtures
 func is_main(node: Node) -> bool:
 	var result = true
-	var children_names = Functions.get_children_names(node)
-	for children_name in children_names:
-		if not children_name.ends_with("Test"):
+	var children = node.get_children()
+	for child in children:
+		if not is_test_fixture(child):
 			result = false
 			break
-	if result:
-		var children = node.get_children()
-		for child in children:
-			if not Functions.get_children_names(child).has("TestCases"):
-				result = false
-				break
 	return result
-	
+
+## Returns test's fixture or null if not found.
 func get_test_fixture() -> Node:
 	var found: bool = false
 	var node = self
@@ -44,17 +43,22 @@ func get_test_fixture() -> Node:
 		found = is_test_fixture(node)
 		if not found:
 			node = node.get_parent()
+	if not found:
+		node = null
 	return node
 
+## Runs all tests implemented by owned test fixtures.
 func run_all_tests():
 	for child in get_children():
 		run_test_fixture(child)
-	
+
+## Runs tests but only for specified test fixture
 func run_test_fixture(node: Node):
 	var test_cases: Array[Node] = node.get_node(TEST_CASES_NODE_NAME).get_children()
 	for test_case in test_cases:
 		run_test_case(test_case)
 
+## Runs test methods for specific test case
 func run_test_case(test_case: Node):
 	assert(test_case, "Test case node can't be null")
 	var children = test_case.get_children()
@@ -65,12 +69,14 @@ func run_test_case(test_case: Node):
 			if Functions.string_starts_with(test_name, TEST_PREFIX):
 				print("%s is calling testing method: %s" % [test_case.name, test_name])
 				test_case.call(test_name)
+				test_case._reset()
 	else:
 		for child in children:
-			if child.has_method("run_test_case"):
+			if child is Testing:
 				print("Running test cases for %s" % child.name)
 				run_test_case(child)
 
+## Main usable function to run all tests contained by test structure.
 func run_tests():
 	if is_main(self):
 		run_all_tests()
--- scripts/tests/utils.gd
+++ scripts/tests/utils.gd
@@ -1,16 +1,87 @@
 extends Node
 
+## Methods and helpers used within Test Framework
 class_name TestMethods
 
 enum Type { EQ, NEQ, SAME_TYPE, BOOL }
 
 const FAILED: bool = false
-const TYPE_TO_STR_INDEX = {
+const _TYPE_TO_STR_INDEX = {
 	Type.EQ: "EQUAL VARIABLES",
 	Type.NEQ: "DIFFERENT VARIABLES",
 	Type.SAME_TYPE: "SAME TYPE",
 	Type.BOOL: "CONDITION FULFILLED"
 }
+
+var _call_status: bool = false
+var _call_status_index: Array[bool] = []
+
+func _reset():
+	_call_status = false
+	_call_status_index = []
+
+## Default index for calls that refers to unique call status
+const UNIQUE_CALL: int = -1
+
+func _validate_call_status_index(index: int):
+	assert(index >= UNIQUE_CALL && index < Constants.MAX_INT, 
+			"Invalid called index (%d). Valid indexes: [%d - %d]" % 
+			[index, UNIQUE_CALL, Constants.MAX_INT - Constants.ZERO_INDEXING_OFFSET])
+
+func _resize_call_index(new_size: int):
+	assert(new_size >= 0 && new_size > _call_status_index.size())
+	var new_calls: Array[bool] = []
+	new_calls.resize(new_size - _call_status_index.size())
+	new_calls.fill(false)
+	_call_status_index.append_array(new_calls)
+
+## Returns callable which, when called. will update call status with index provided by user(unique by default)
+func CALL_STATUS_UPDATE(index: int = UNIQUE_CALL) -> Callable:
+	_validate_call_status_index(index)
+	if index == UNIQUE_CALL:
+		return func(): _call_status = true
+	else:
+		if _call_status_index.size() <= index:
+			_resize_call_index(index + 1)
+		return func(): _call_status_index[index] = true
+
+func _warn_index_out_of_call_status_array_boundaries(index: int):
+	var array_boundaries_str: String = "EMPTY"
+	if _call_status_index.size() > 0:
+		array_boundaries_str = "%d - %d" % [0, _call_status_index.size() - Constants.ZERO_INDEXING_OFFSET]
+	push_warning("Index (%d) out of call status array boundaries [%s]. Returning FALSE." % 
+			[index, array_boundaries_str])
+
+## Sets call status with index provided by user to false (not called)
+func RESET_CALL(index: int = UNIQUE_CALL):
+	_validate_call_status_index(index)
+	if index == UNIQUE_CALL:
+		_call_status = false
+	else:
+		if _call_status_index.size() <= index:
+			return
+		_call_status_index[index] = false
+
+## Sets every call status to false (not called)
+func RESET_ALL_CALLS():
+	_call_status = false
+
+	var size = _call_status_index.size()
+	_call_status_index.resize(0)
+	_call_status_index.resize(size)
+	_call_status_index.fill(false)
+
+## Check call status with index provided by user (unique by default). Returns false if never updated
+## or index is out of status array boundaries.
+func CALLED(index: int = UNIQUE_CALL) -> bool:
+	_validate_call_status_index(index)
+	if index == UNIQUE_CALL:
+		return _call_status
+	else:
+		if _call_status_index.size() <= index:
+			_warn_index_out_of_call_status_array_boundaries(index)
+			return false
+		return _call_status_index[index]
 
 func _add_status_to_current_case(_status: bool):
 	pass
@@ -36,7 +107,7 @@ func _trim_stack(stack: PackedStringArray, elements: int) -> String:
 func _eq_error(var1, var2, type: Type):
 	var msg: String = "
 	TEST FAILED!
-	EXPECTED: " + TYPE_TO_STR_INDEX[type] + "
+	EXPECTED: " + _TYPE_TO_STR_INDEX[type] + "
 	VAR_1 = \"" + str(var1) + "\"" + "
 	VAR_2 = \"" + str(var2) + "\"" + "
 	PATH: " + _trim_stack(get_stack(), 3)
@@ -45,7 +116,7 @@ func _eq_error(var1, var2, type: Type):
 func _diff_type_error():
 	var msg: String = "
 	TEST FAILED!
-	EXPECTED: " + TYPE_TO_STR_INDEX[Type.SAME_TYPE] + "
+	EXPECTED: " + _TYPE_TO_STR_INDEX[Type.SAME_TYPE] + "
 	VAR_1 TYPE != VAR_2 TYPE" + "
 	PATH: " + _trim_stack(get_stack(), 4)
 	push_error(msg)
@@ -54,23 +125,28 @@ func _diff_type_error():
 func _bool_error(cond: bool):
 	var msg: String = "
 	TEST FAILED!
-	EXPECTED: " + TYPE_TO_STR_INDEX[Type.BOOL] +" (" + str(cond).to_upper() + ")" + "
+	EXPECTED: " + _TYPE_TO_STR_INDEX[Type.BOOL] +" (" + str(cond).to_upper() + ")" + "
 	CONDITION = " + str(not cond).to_upper() + "
 	PATH: " + _trim_stack(get_stack(), 3)
 	push_error(msg)	
 
+## Expects that both variables are equal, throws error otherwise.
 func EXPECT_EQ(var1, var2):
 	_EXPECT_EQ(var1, var2, Type.EQ)
-
+	
+## Expects that both variables are not equal, throws error otherwise.
 func EXPECT_NEQ(var1, var2):
 	_EXPECT_EQ(var1, var2, Type.NEQ)
 
+## Expects that condition is fulfilled, throws error otherwise.
 func EXPECT_TRUE(condition: bool):
 	_EXPECT_BOOL(condition, true)
 
+## Expects that condition is not fulfilled, throws error otherwise.
 func EXPECT_FALSE(condition: bool):
 	_EXPECT_BOOL(condition, false)
 
+## Returns count that can't be negative. Just a syntax helper.
 func TIMES(count: int) -> int:
 	if count < 0:
 		count = 0
@@ -81,6 +157,7 @@ func _EXPECT_BOOL(condition: bool, expected: bool):
 	if status == FAILED:
 		_bool_error(expected)
 
+## Expects that both variable types are the same, throws error otherwise.
 func EXPECT_SAME_TYPE(var1, var2) -> bool:
 	var status: bool = typeof(var1) == typeof(var2)
 	#TBD 
